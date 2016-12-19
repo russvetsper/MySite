434c500b3b14f5f08dcc0adf11ef18eda93413c4
russvetsper <russvetsper@gmail.com>
2016-12-18 21:36:21 -0800

    api call add to project

--- src/MySite/Models/Repo.cs
+++ src/MySite/Models/Repo.cs
@@ -17,33 +17,38 @@ namespace MySite.Models
 
         public static List<Repo> GetRepos()
         {
-            
-            var client = new RestClient("https://api.github.com/search/repositories?page=1&q=user:russvetsper&sort=stars:>0&order=desc");
-            var request = new RestRequest("", Method.GET);
-            request.AddParameter("Access_token", EnvironmentVariables.AuthToken);
+
+            var client = new RestClient("https://api.github.com/search/repositories?page=1&q=user:russvetsper&sort=stars:>=1&order=desc");
+            var request = new RestRequest(/*"Accounts/" + EnvironmentVariables.AccountSid + "/Projects.json",*/ Method.GET);
+
+           
             request.AddHeader("User-Agent", "russvetsper");
-            request.AddHeader("Accept", "application/vnd.github.v3.text-match+json");
-            client.Authenticator = new HttpBasicAuthenticator("/Itmes.json", EnvironmentVariables.AuthToken);
+           
+            request.AddHeader("Accept", "application/vnd.github.v3.+json");
+
+
             var response = new RestResponse();
             Task.Run(async () =>
             {
                 response = await GetResponseContentAsync(client, request) as RestResponse;
             }).Wait();
 
-            
+
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            
             var repoList = JsonConvert.DeserializeObject<List<Repo>>(jsonResponse["items"].ToString());
             return repoList;
-
         }
-       
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
         {
             var tcs = new TaskCompletionSource<IRestResponse>();
-            theClient.ExecuteAsync(theRequest, response => {
+            theClient.ExecuteAsync(theRequest, response =>
+            {
                 tcs.SetResult(response);
             });
             return tcs.Task;
+
         }
+
     }
 }
--- src/MySite/Startup.cs
+++ src/MySite/Startup.cs
@@ -7,27 +7,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace MySite
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            //1
             services.AddMvc();
         }
-
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
@@ -37,9 +36,14 @@ namespace MySite
             });
 
 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Bad Bad!");
+                await context.Response.WriteAsync(" error.");
             });
         }
     }
